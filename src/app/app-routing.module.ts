00177d85d3ded67c52548593b6855482bec40edb
Roshan COTCHE <rcotche.cdi@FORMATION.LOCAL>
2019-02-07 11:45:58 +0400

    clean route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FaqComponent } from './faq/faq.component' ;
 import { ProfileUserComponent } from './profile-user/profile-user.component';
-import { GalleryComponent } from './gallery/gallery.component';  
+import { GalleryComponent } from './gallery/gallery.component';
 import { AccueilComponent } from './accueil/accueil.component';
 
 const routes: Routes = [
-  {path:'faq',component: FaqComponent  },
   {path: '', pathMatch: 'full', component: AccueilComponent},
   {path: 'gallery', component: GalleryComponent},
-  {path: 'rv', component: ProfileUserComponent}
+  {path: 'rv', component: ProfileUserComponent},
+  {path: 'faq', component: FaqComponent  }
 ];
 
 @NgModule({
