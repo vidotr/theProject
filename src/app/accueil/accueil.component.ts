51b2cca10216480b901153273ae16fe5ac191576
Roshan COTCHE <rcotche.cdi@FORMATION.LOCAL>
2019-02-08 11:54:11 +0400

    categori class add

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Categorie } from '../categorie';
 
 @Component({
   selector: 'app-accueil',
---
+++ src/app/categorie.ts
@@ -0,0 +1,5 @@
+export class Categorie {
+    nom: string;
+    description: string;
+    favori: boolean;
+}
