7a1cca04757215dc06c0431971795871c6f7c785
Roshan COTCHE <rcotche.cdi@FORMATION.LOCAL>
2019-02-08 14:30:42 +0400

    accueil en cours

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -11,9 +11,9 @@ export class AccueilComponent implements OnInit {
     // ListCategory: Array<Categorie>;
     connectee: boolean;
     ListCategoryTest: Categorie[] = [
-        { nom: 'Technologie', description: 'Toutes les nouveauté sur la technologie', image: 'One', favori: false },
+        { nom: 'Technologie', description: 'Toutes les nouveauté sur la technologie', image: 'a.jpeg', favori: false },
         { nom: 'K-pop', description: 'Toutes les nouveauté sur la K-pop', image: 'One', favori: false },
-        { nom: 'Drama', description: 'Toutes les nouveauté sur les Dramas', image: 'One', favori: true },
+        { nom: 'Drama', description: 'Toutes les nouveauté sur les Dramas', image: 'pdp.png', favori: true },
         { nom: 'Anime', description: 'Toutes les nouveauté sur les Animés', image: 'One', favori: false },
     ];
 
