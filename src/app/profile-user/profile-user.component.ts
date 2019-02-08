b33ba7d0a6a7eee13bb5c8a0bc742b301bb536ff
Ruben VIDOT <rvidot.cdi@FORMATION.LOCAL>
2019-02-08 11:06:04 +0400

    subjects

--- src/app/profile-user/profile-user.component.ts
+++ src/app/profile-user/profile-user.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tile {
+    title: string;
+    text: string;
+}
+
 @Component({
     selector: 'app-profile-user',
     templateUrl: './profile-user.component.html',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileUserComponent implements OnInit {
     panelOpenState = false;
+    nb = 1;
+
+    tiles: Tile[] = [
+        { title: 'One', text: 'lightblue' },
+        { title: 'Two', text: 'lightgreen' },
+        { title: 'Three', text: 'lightpink' },
+        { title: 'Four', text: 'lightpink' },
+    ];
     constructor() { }
 
     ngOnInit() {
     }
 
+    /*ngClassForCarousselItem() {
+        this.nb += 1;
+        let data = '';
+        if (this.nb === 1) {
+            data = 'carousel-item active';
+        } else {
+            data = 'carousel-item';
+        }
+        return data;
+    }*/
+    checkNb() {
+        this.nb += 1;
+    }
 }
